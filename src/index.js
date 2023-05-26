601d56af681a830cdc1542ff9b12d8004af89b77
Alexander Uzunov <kaialexander.11@outlook.com>
2023-05-26 13:02:27 +0300

    Add static assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 //console.log('Hello world');
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
  
 const app = express();
 
 const PORT = 5000;
+
+//Express config
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 // Handlebars config
 app.engine('hbs', handlebars.engine({
