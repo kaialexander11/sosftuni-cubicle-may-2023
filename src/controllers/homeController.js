1fa528a4cac1961e470155000892e4e7ab12ac5e
Alexander Uzunov <kaialexander.11@outlook.com>
2023-05-29 16:12:09 +0300

    Add cube details page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -13,4 +13,6 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+
+
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,15 +1,32 @@
 const uniqid = require('uniqid');
 
-const cubes = [];
+const cubes = [
+    {
+        id: '1nbo9fozli8rf0ud',
+        name: 'Mirror Cube',
+        description: 'Light Reflecting Cube!',
+        imageUrl: 'https://cdn.wallpapersafari.com/91/59/TGQejB.jpg',
+        difficultyLevel: 4
+    },
+
+    {
+        id: '1nbo9fozli8rf0ud',
+        name: 'Rubic Classic Cube',
+        description: 'Evergreen Cube!',
+        imageUrl: 'https://img2.cgtrader.com/items/642213/847af60587/rubic-cube-3d-model-max-obj-mtl-3ds-fbx-dxf.jpg',
+        difficultyLevel: 3
+    }
+
+];
 
 exports.getAll = () => cubes.slice();
 
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+
+
+
 exports.create = (cubeData) => {
     const newCube = {
-        //valid lines:
-        // id: cubes[cubes.length - 1].id + 1,
-        // ...cubeData,
-
         id: uniqid(),
         ...cubeData,
     };
