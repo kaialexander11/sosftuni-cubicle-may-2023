5311ef2c5582b754b36f1001120175fc97e238a8
Alexander Uzunov <kaialexander.11@outlook.com>
2023-05-30 10:15:22 +0300

    Extract routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 //console.log('Hello world');
 const express = require('express');
 //const path = require('path');
-
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const routes = require('./routes');
  
 const app = express();
 
@@ -13,12 +11,10 @@ const PORT = 5000;
 
 expressConfig(app);
 handlebarsConfig(app);
-//require('./config/expressConfig')(app); => valid, but old, same as the one above
 
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req, res) => {
-    res.redirect('/404');
-});
+app.use(routes);
+
+//require('./config/expressConfig')(app); => valid, but old, same as the one above
 //app.get('/', homeController.getHome);
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
